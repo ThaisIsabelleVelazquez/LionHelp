d3463e600858b2933b705488ffc5adead454d268
ThaisIsabelleVelazquez <tiv2101@barnard.edu>
2025-12-01 18:40:35 -0500

    changes made for search feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,21 +2,26 @@ class ReviewsController < ApplicationController
   before_action :require_login
 
   def index
+
     @reviews = Review.all
 
+ 
     if params[:filter] == "about_me_vendor" && session[:user_name].present?
       @reviews = @reviews.where(reviewer: "client", vendor_name: session[:user_name])
-
     elsif params[:filter] == "written_by_me" && session[:user_name].present?
-      @reviews = @reviews.where(client_name: session[:user_name], reviewer: "client").or(
-        @reviews.where(vendor_name: session[:user_name], reviewer: "vendor")
-      )
-
+      @reviews = @reviews.where(client_name: session[:user_name], reviewer: "client")
+                         .or(@reviews.where(vendor_name: session[:user_name], reviewer: "vendor"))
     elsif params[:filter] == "vendor"
       @reviews = @reviews.where(reviewer: "vendor")
-
     elsif params[:filter] == "client"
       @reviews = @reviews.where(reviewer: "client")
+    end
+
+ 
+    if params[:q].present?
+    
+      @search = Review.ransack(title_or_comment_cont: params[:q])
+      @reviews = @search.result
     end
   end
 
@@ -24,55 +29,51 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-def create
-  if params[:review][:reviewer] == 'vendor'
-    params[:review][:vendor_name] = params[:review][:client_name]
-    params[:review][:client_name] = session[:user_name]  
-    params[:review][:reviewer] = 'client'
-  elsif params[:review][:reviewer] == 'client'
+  def create
+    if params[:review][:reviewer] == 'vendor'
+      params[:review][:vendor_name] = params[:review][:client_name]
+      params[:review][:client_name] = session[:user_name]  
+      params[:review][:reviewer] = 'client'
+    elsif params[:review][:reviewer] == 'client'
+      params[:review][:vendor_name] = session[:user_name]  
+      params[:review][:reviewer] = 'vendor'
+    end
 
-    params[:review][:vendor_name] = session[:user_name]  
-    params[:review][:reviewer] = 'vendor'
+    @review = Review.new(review_params)
+
+    if @review.rating.to_i > 10
+      flash[:notice] = "Rating is too high."
+      redirect_to new_review_path and return
+    elsif @review.rating.to_i < 0
+      flash[:notice] = "Rating is too low."
+      redirect_to new_review_path and return
+    end
+
+    if @review.save
+      flash[:notice] = "Review posted successfully!"
+      redirect_to reviews_path
+    else
+      flash[:notice] = "Error saving review: " + @review.errors.full_messages.join(", ")
+      redirect_to new_review_path
+    end
   end
 
-  @review = Review.new(review_params)
+  def destroy
+    review = Review.find(params[:id])
+    current_user = UserAccount.find_by(name: session[:user_name])
+    vendor_account = UserAccount.find_by(name: review.vendor_name)
+    client_account = UserAccount.find_by(name: review.client_name)
 
-  if @review.rating.to_i > 10
-    flash[:notice] = "Rating is too high."
-    redirect_to new_review_path and return
-  elsif @review.rating.to_i < 0
-    flash[:notice] = "Rating is too low."
-    redirect_to new_review_path and return
+    if (review.reviewer == "vendor" && vendor_account&.email == current_user.email) ||
+       (review.reviewer == "client" && client_account&.email == current_user.email)
+      review.destroy
+      flash[:notice] = "Review deleted"
+    else
+      flash[:alert] = "You can only delete your own reviews."
+    end
+
+    redirect_to reviews_path(filter: "written_by_me")
   end
-
-  if @review.save
-    flash[:notice] = "Review posted successfully!"
-    redirect_to reviews_path
-  else
-    flash[:notice] = "Error saving review: " + @review.errors.full_messages.join(", ")
-    redirect_to new_review_path
-  end
-end
-
-
-
-def destroy
-  review = Review.find(params[:id])
-
-  current_user = UserAccount.find_by(name: session[:user_name])
-  vendor_account = UserAccount.find_by(name: review.vendor_name)
-  client_account = UserAccount.find_by(name: review.client_name)
-  if (review.reviewer == "vendor" && vendor_account&.email == current_user.email) ||
-     (review.reviewer == "client" && client_account&.email == current_user.email)
-    review.destroy
-    flash[:notice] = "Review deleted"
-  else
-    flash[:alert] = "You can only delete your own reviews."
-  end
-
-  redirect_to reviews_path(filter: "written_by_me")
-end
-
 
   def review_params
     params.require(:review).permit(:vendor_name, :client_name, :title, :rating, :comment, :reviewer)
@@ -92,7 +93,6 @@ end
 
   def edit
     @review = Review.find(params[:id])
-    puts @review
     if @review[:reviewer] == "client"
       @role = "Vendor"
       @user_of_interest = UserAccount.find_by(user_id: @review[:vendor_id])
