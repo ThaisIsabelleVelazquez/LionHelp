2529c207ae7e557ee2eb2e1c918e61ced4a468e1
emilyburnett2003 <118637936+emilyburnett2003@users.noreply.github.com>
2025-12-11 07:56:19 -0500

    only allow strong passwords

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -2,6 +2,7 @@ class UserAccount < ActiveRecord::Base
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: { message: "email already exists" }
   validate :is_columbia_email
+  validate :strong_password
 
   def is_columbia_email
     if email.blank?
@@ -11,4 +12,14 @@ class UserAccount < ActiveRecord::Base
       errors.add(:email, "must be a valid Columbia or Barnard email address")
     end
   end
+  
+  def strong_password
+    if password.blank?
+      return
+    end
+    unless self.password.match?(/\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}\z/)
+      errors.add(:password, "must be a strong password that is at least 8 characters and includes a lowercase letter, an uppercase letter, a number, and a special character")
+    end
+  end
 end
+
