8ef0fe5395c0f0acc520408e9c317a048a29a290
ThaisIsabelleVelazquez <tiv2101@barnard.edu>
2025-12-13 22:54:37 -0500

    to work with the profile display

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
 
-  resources :useraccounts, only: [:show]   
-  resources :users, only: [:show]
+  get '/profile', to: 'accounts#profile', as: 'profile'
+
 
 
   resources :reviews
