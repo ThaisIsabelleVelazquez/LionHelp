26014fdb3e02cb513c0d63b6933b8902d4033858
ThaisIsabelleVelazquez <tiv2101@barnard.edu>
2025-12-13 22:22:44 -0500

    search reviews are on the review page

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
 
-  
-  get "search/reviews",  to: "search#reviews"
-  get "search/services", to: "search#services"
-
-  
-
   resources :reviews
   get '/vendor_reviews/:vendor_id', to: 'reviews#show_vendor_reviews', as: 'vendor_reviews'
   get '/client_reviews/:client_id', to: 'reviews#show_client_reviews', as: 'client_reviews'
